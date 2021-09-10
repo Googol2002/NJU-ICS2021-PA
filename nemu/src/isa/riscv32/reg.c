505a7eaa254eebc09a1fbfd36c2b3ec9ed794b51
201300052-Shi Haoyu <xingyuncao2002@outlook.com>
2021-09-09 21:14:54 -0700

    编写了isa_reg_display

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -9,6 +9,11 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+  length = ARRLEN(regs);
+  printf("=========寄存器信息=========")
+  for (int i < 0; i < length; ++i){
+    printf("%s: %x", regs[i], cpu.gpr[i]._32]);
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
