4fef57953d0158cf456ed4972b98453e1bd70335
201300052-Shi Haoyu <xingyuncao2002@outlook.com>
2021-09-11 20:48:04 -0700

    添加了gen-expr功能

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -16,8 +16,47 @@ static char *code_format =
 "  return 0; "
 "}";
 
+
+int choose(int n){
+  return rand() % n;
+}
+
+void gen(char c){
+  char cha_buffer[2] = {c, '\0'};
+  strcat(buf, cha_buffer);
+}
+
+void gen_rand_op(){
+  switch (choose(4)){
+    case 0:
+      gen('+');
+      break;
+    case 1:
+      gen('-');
+      break;
+    case 2:
+      gen('*');
+      break;
+    case 3:
+      gen('/');
+      break;
+  }
+}
+
+void gen_num(){
+  char num_buffer[1024];
+  num_buffer[0] = '\0';
+  sprintf(num_buffer ,"%d", rand() % 10000);
+  strcat(buf, num_buffer);
+}
+
 static void gen_rand_expr() {
   buf[0] = '\0';
+  switch (choose(3)) {
+    case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -30,7 +69,7 @@ int main(int argc, char *argv[]) {
   int i;
   for (i = 0; i < loop; i ++) {
     gen_rand_expr();
-
+    printf("expr: %s \n", buf);
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
@@ -45,7 +84,8 @@ int main(int argc, char *argv[]) {
     assert(fp != NULL);
 
     int result;
-    fscanf(fp, "%d", &result);
+    if (fscanf(fp, "%d", &result) != 1)
+      assert(0);
     pclose(fp);
 
     printf("%u %s\n", result, buf);
