9fccd1c18377ffe48526dc06b9612738590a8a06
tracer-ics2021 <tracer@njuics.org>
2021-10-09 16:51:47 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 50a1f236d4ca4b558bc219a93fd7b5be     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  16:51:47 up  2:53,  2 users,  load average: 0.27, 0.16, 0.07

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -13,18 +13,13 @@ override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
-IMG ?=
+IMG ?= ~/PA/ics2021/am-kernels/tests/cpu-tests/build/dummy-riscv32-nemu.bin
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
 	$(call git_commit, "run")
-	$(NEMU_EXEC)
-
-img: run-env
-	$(call git_commit, "run")
-	$(ARGS += ~/PA/ics2021/am-kernels/tests/cpu-tests/build/dummy-riscv32-nemu.bin)
 	$(NEMU_EXEC)
 
 gdb: run-env
