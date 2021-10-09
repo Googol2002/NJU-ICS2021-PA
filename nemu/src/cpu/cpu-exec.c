c832818f627c5a69074bbbbf703f8248ed17eaa9
tracer-ics2021 <tracer@njuics.org>
2021-10-09 14:22:46 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 50a1f236d4ca4b558bc219a93fd7b5be     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  14:22:46 up 24 min,  2 users,  load average: 0.08, 0.02, 0.09

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -30,11 +30,13 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc){
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   
-  WP* point = NULL;
-  if (check_watchpoint(&point)){
-    printf("Stoped at \e[1;36mWatchPoint(NO.%d)\e[0m: %s \n", point->NO, point->condation);
-    nemu_state.state = NEMU_STOP;
-  }
+  #ifdef CONFIG_DEBUG
+    WP* point = NULL;
+    if (check_watchpoint(&point)){
+      printf("Stoped at \e[1;36mWatchPoint(NO.%d)\e[0m: %s \n", point->NO, point->condation);
+      nemu_state.state = NEMU_STOP;
+    }
+  #endif
 }
 
 #include <isa-exec.h>
