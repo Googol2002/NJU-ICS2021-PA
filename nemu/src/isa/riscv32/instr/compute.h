caff53aa9a13e2b98f2771ac26e197460daa29c2
tracer-ics2021 <tracer@njuics.org>
2021-10-12 23:48:42 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: bb592477e1b045b88bfcd1930410662a     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  23:48:42 up  2:59,  4 users,  load average: 0.11, 0.07, 0.02

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -12,6 +12,10 @@ def_EHelper(andi) {
   rtl_andi(s, ddest, dsrc1, id_src2->imm);
 }
 
+def_EHelper(xori) {
+  rtl_xori(s, ddest, dsrc1, id_src2->imm);
+}
+
 def_EHelper(sll) {
   rtl_sll(s, ddest, dsrc1, dsrc2);
 }
