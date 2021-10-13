f76b6303c01c1f66663ed43dd9149c5c8e8c8d80
tracer-ics2021 <tracer@njuics.org>
2021-10-13 15:01:23 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 7c4540c9d11240cf8fa9970f2f1dd635     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  15:01:23 up  1:21,  4 users,  load average: 0.28, 0.13, 0.10

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -115,8 +115,9 @@ def_EHelper(jal) {
 
 def_EHelper(jalr) {
   Log("jalr");
-  rtl_addi(s, s0, &s->pc, 4);
+  word_t buffer = s->pc + 4;
+  // rtl_addi(s, s0, &s->pc, 4);
   rtl_addi(s, &s->dnpc, dsrc1, id_src2->imm);
   rtl_andi(s, &s->dnpc, &s->dnpc, ~1);
-  rtl_addi(s, ddest, s0, 0);
+  rtl_addi(s, ddest, &buffer, 0);
 }
