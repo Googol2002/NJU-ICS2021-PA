205ebac490f05c32bd199639df0231a30981fe1d
tracer-ics2021 <tracer@njuics.org>
2021-10-13 18:37:46 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 7c4540c9d11240cf8fa9970f2f1dd635     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  18:37:46 up  3:36,  2 users,  load average: 0.02, 0.05, 0.03

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -4,6 +4,6 @@
 #define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(addi) f(auipc) f(jal) f(jalr) f(nemu_trap) \
 f(add) f(beq) f(sub) f(mul) f(sltiu) f(bne) f(sltu) f(xor) f(or) f(and) f(sb) f(sra) f(srl) \
 f(srai) f(srli) f(lbu) f(andi) f(sll) f(xori) f(bge) f(div) f(rem) f(blt) f(slt) f(lh) f(lhu) \
-f(slli)
+f(slli) f(sh)
 
 def_all_EXEC_ID();
--- nemu/src/isa/riscv32/instr/ldst.h
+++ nemu/src/isa/riscv32/instr/ldst.h
@@ -18,7 +18,10 @@ def_EHelper(sw) {
   rtl_sm(s, ddest, dsrc1, id_src2->imm, 4);
 }
 
-
 def_EHelper(sb) {
   rtl_sm(s, ddest, dsrc1, id_src2->imm, 1);
+}
+
+def_EHelper(sh) {
+  rtl_sm(s, ddest, dsrc1, id_src2->imm, 2);
 }
