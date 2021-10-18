fa970b25bf416548c114b0e624591407f96136d3
tracer-ics2021 <tracer@njuics.org>
2021-10-18 19:58:26 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 39b57c605cf9408d8ce9ee18308540c6     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  19:58:26 up 29 min,  2 users,  load average: 0.00, 0.04, 0.04

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -13,7 +13,7 @@ override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
-IMG ?= ~/PA/ics2021/am-kernels/tests/cpu-tests/build/add-riscv32-nemu.bin
+IMG ?= ~/PA/ics2021/am-kernels/tests/cpu-tests/build/if-else-riscv32-nemu.bin
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -39,7 +39,7 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   #ifdef CONFIG_MTRACE
-    Log("Read from memory at %16X for %d bytes.", addr, len);
+    Log(" Read  from memory at %16X for %d bytes.", addr, len);
   #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   MUXDEF(CONFIG_DEVICE, return mmio_read(addr, len),
@@ -49,7 +49,7 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   #ifdef CONFIG_MTRACE
-    Log("Write to memory at %16X for %d bytes.", addr, len);
+    Log(" Write to memory at %16X for %d bytes.", addr, len);
   #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   MUXDEF(CONFIG_DEVICE, mmio_write(addr, len, data),
