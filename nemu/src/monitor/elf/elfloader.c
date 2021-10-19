7ee580da2dc394e08b718be16a2daa3b22783d3b
tracer-ics2021 <tracer@njuics.org>
2021-10-19 19:46:31 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 7add812239f64decaf3bacaa01413e76     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  19:46:31 up 24 min,  2 users,  load average: 0.29, 0.15, 0.11

--- nemu/src/monitor/elf/elfloader.c
+++ nemu/src/monitor/elf/elfloader.c
@@ -3,6 +3,11 @@
 
 #include "elfloader.h"
 
+// typedef Elf32_Shdr Elf_Shdr;
+// typedef Elf32_Ehdr Elf_Ehdr;
+
+
+
 static void read_from_file(FILE *elf, size_t offset, size_t size, void* dest){
     fseek(elf, offset, SEEK_SET);
     int flag = fread(dest, size, 1, elf);
@@ -15,11 +20,35 @@ void init_elf(const char* path){
     Elf32_Ehdr elf_header;
     read_from_file(elf, 0, sizeof elf_header, &elf_header);
 
-    Elf32_Off selection_header_offset = elf_header.e_shoff;
+    Elf32_Off section_header_offset = elf_header.e_shoff;
     size_t headers_entry_size = elf_header.e_shentsize;
     int headers_entry_num = elf_header.e_shnum;
 
     printf("====== Reading ELF File ======\n");
-    printf("e_shoff: %d \n", selection_header_offset);
+    printf("e_shoff: %d \n", section_header_offset);
     printf("e_shentsize: %ld\t e_shnum: %d \n", headers_entry_size, headers_entry_num);
+    
+    assert(sizeof(Elf32_Shdr) == headers_entry_size);
+    
+    printf("====== Selection Headers ======\n");
+    
+    Elf32_Off string_table_offset, symbol_table_offset;
+    for (int i = 0; i < headers_entry_num; ++i){
+        Elf32_Shdr section_entry;
+        read_from_file(elf, i * headers_entry_size + section_header_offset,
+            headers_entry_size, &section_entry);
+        switch(section_entry.sh_type){
+            case SHT_SYMTAB:
+                symbol_table_offset = section_entry.sh_offset;
+            break;
+
+            case SHT_STRTAB:
+                string_table_offset = section_entry.sh_offset;
+            break;
+        }
+    }
+    printf("String Table Offset: %#x\n", string_table_offset);
+    printf("Symbol Table Offset: %#x\n", symbol_table_offset);
+    
+
 }
