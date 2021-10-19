fe767c5530c0e310109ec4133de648de9eb9a7e1
tracer-ics2021 <tracer@njuics.org>
2021-10-19 15:10:39 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 7bb3288932c24c0cbd298c9325da3ddb     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  15:10:39 up 44 min,  2 users,  load average: 0.37, 0.27, 0.26

--- nemu/src/monitor/elf/elfloader.c
+++ nemu/src/monitor/elf/elfloader.c
@@ -15,6 +15,6 @@ void init_elf(const char* path){
     Elf32_Ehdr elf_header;
     read_from_file(elf, 0, sizeof elf_header, &elf_header);
 
-    printf("e_shoff: %x", elf_header.e_shoff);
-    printf("e_shentsize: %d\t e_shnum: %d", elf_header.e_shentsize, elf_header.e_shnum);
+    printf("e_shoff: %x \n", elf_header.e_shoff);
+    printf("e_shentsize: %d\t e_shnum: %d \n", elf_header.e_shentsize, elf_header.e_shnum);
 }
