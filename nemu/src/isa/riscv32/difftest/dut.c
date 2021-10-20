afdcba8d07f5cb250ccd66632114a47130ba850e
tracer-ics2021 <tracer@njuics.org>
2021-10-20 19:59:08 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 5eaa775a9f4549aeb7ad12115e66095f     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  19:59:09 up  2:12,  3 users,  load average: 0.08, 0.19, 1.04

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -3,7 +3,12 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for (int i = 0; i < 32; ++i){
+    if (ref_r->gpr[i]._32 != cpu.gpr[i]._32)
+      return false;
+  }
+
+  return ref_r->pc == pc;
 }
 
 void isa_difftest_attach() {
