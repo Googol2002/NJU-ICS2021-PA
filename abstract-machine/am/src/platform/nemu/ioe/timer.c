a813eeeed2ede1fabfc80498070831450e6787b9
tracer-ics2021 <tracer@njuics.org>
2021-10-21 20:02:00 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 7cc89116068046f4809cb47a087006dd     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:02:00 up  4:24,  2 users,  load average: 0.08, 0.06, 0.01

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,11 +1,12 @@
 #include <am.h>
 #include <nemu.h>
 
-void __am_timer_init() {
-}
+#include "../../../riscv/riscv.h"
 
-void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+void __am_timer_init() {}
+
+void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {\
+  uptime->us = (uint64_t)inl(RTC_ADDR) | ((uint64_t)inl(RTC_ADDR + 4) << 32);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
