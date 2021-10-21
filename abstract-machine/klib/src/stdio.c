53dd69a86f7ab848e085187686b94dbd5b5c8120
tracer-ics2021 <tracer@njuics.org>
2021-10-21 15:13:24 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 7cc89116068046f4809cb47a087006dd     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  15:13:24 up  1:35,  3 users,  load average: 0.14, 0.17, 0.27

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,17 +5,21 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-// int printf(const char *fmt, ...) {
-//   panic("Not implemented");
-// }
+int vsprintf(char *out, const char *fmt, va_list ap);
 
-// int vsprintf(char *out, const char *fmt, va_list ap) {
-//   panic("Not implemented");
-// }
+int printf(const char *fmt, ...) {
+  char buffer[1024];
+  va_list arg;
+  va_start (arg, fmt);
+  
+  int done = vsprintf(buffer, fmt, arg);
 
-int sprintf(char *out, const char *fmt, ...) {
-  va_list valist;
-  va_start(valist, fmt);
+  putstr(buffer);
+
+  return done;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
   char buffer[128];
   char *txt;
   int num, len;
@@ -35,13 +39,13 @@ int sprintf(char *out, const char *fmt, ...) {
       switch (fmt[i])
       {
       case 's':
-        txt = va_arg(valist, char*);
+        txt = va_arg(ap, char*);
         for (int k = 0; txt[k] !='\0'; ++k)
           out[j++] = txt[k];
         break;
       
       case 'd':
-        num = va_arg(valist, int);
+        num = va_arg(ap, int);
         for (len = 0; num ; num /= 10, ++len)
           buffer[len] = num % 10 + '0';//逆序的
         for (int k = len - 1; k >= 0; --k)
@@ -54,8 +58,53 @@ int sprintf(char *out, const char *fmt, ...) {
   }
 
   out[j] = '\0';
-  va_end(valist);
   return j;
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list valist;
+  va_start(valist, fmt);
+  // char buffer[128];
+  // char *txt;
+  // int num, len;
+  
+  // int state = 0, i, j;//模仿一个状态机
+  // for (i = 0, j = 0; fmt[i] != '\0'; ++i){
+  //   switch (state)
+  //   {
+  //   case 0:
+  //     if (fmt[i] != '%')
+  //       out[j++]=fmt[i];
+  //     else
+  //       state = 1;
+  //     break;
+    
+  //   case 1:
+  //     switch (fmt[i])
+  //     {
+  //     case 's':
+  //       txt = va_arg(valist, char*);
+  //       for (int k = 0; txt[k] !='\0'; ++k)
+  //         out[j++] = txt[k];
+  //       break;
+      
+  //     case 'd':
+  //       num = va_arg(valist, int);
+  //       for (len = 0; num ; num /= 10, ++len)
+  //         buffer[len] = num % 10 + '0';//逆序的
+  //       for (int k = len - 1; k >= 0; --k)
+  //         out[j++] = buffer[k];
+  //       break;
+  //     }
+  //     state = 0;
+  //     break;
+  //   }
+  // }
+
+  // out[j] = '\0';
+  int res = vsprintf(out ,fmt, valist);
+  va_end(valist);
+  return res;
 }
 
 // int snprintf(char *out, size_t n, const char *fmt, ...) {
