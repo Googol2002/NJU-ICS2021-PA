59d46841fff168bfa198744106a68ba40c5e4348
tracer-ics2021 <tracer@njuics.org>
2021-10-21 20:46:54 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 7cc89116068046f4809cb47a087006dd     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:46:54 up  5:09,  3 users,  load average: 0.08, 0.07, 0.03

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -64,44 +64,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 int sprintf(char *out, const char *fmt, ...) {
   va_list valist;
   va_start(valist, fmt);
-  // char buffer[128];
-  // char *txt;
-  // int num, len;
-  
-  // int state = 0, i, j;//模仿一个状态机
-  // for (i = 0, j = 0; fmt[i] != '\0'; ++i){
-  //   switch (state)
-  //   {
-  //   case 0:
-  //     if (fmt[i] != '%')
-  //       out[j++]=fmt[i];
-  //     else
-  //       state = 1;
-  //     break;
-    
-  //   case 1:
-  //     switch (fmt[i])
-  //     {
-  //     case 's':
-  //       txt = va_arg(valist, char*);
-  //       for (int k = 0; txt[k] !='\0'; ++k)
-  //         out[j++] = txt[k];
-  //       break;
-      
-  //     case 'd':
-  //       num = va_arg(valist, int);
-  //       for (len = 0; num ; num /= 10, ++len)
-  //         buffer[len] = num % 10 + '0';//逆序的
-  //       for (int k = len - 1; k >= 0; --k)
-  //         out[j++] = buffer[k];
-  //       break;
-  //     }
-  //     state = 0;
-  //     break;
-  //   }
-  // }
 
-  // out[j] = '\0';
   int res = vsprintf(out ,fmt, valist);
   va_end(valist);
   return res;
