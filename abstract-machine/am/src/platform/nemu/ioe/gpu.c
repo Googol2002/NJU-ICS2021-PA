6be17fba944a060d45867b4853868ff0a5fb8953
tracer-ics2021 <tracer@njuics.org>
2021-10-25 20:48:24 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 433b36cc537e4b08bfc83dcd904f94d8     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:48:24 up  1:11,  1 user,  load average: 0.34, 0.38, 0.37

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -12,6 +12,7 @@ void __am_gpu_init() {
   int i;
   int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
   int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
+  printf("%d, %d", w, h);
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
