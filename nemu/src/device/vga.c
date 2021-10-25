d5c93c95f916be3d36b76e7e102852e0e44a03a0
tracer-ics2021 <tracer@njuics.org>
2021-10-25 21:21:49 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 433b36cc537e4b08bfc83dcd904f94d8     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  21:21:49 up  1:45,  1 user,  load average: 2.05, 1.21, 0.77

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -56,7 +56,6 @@ static inline void update_screen() {
 #endif
 
 void vga_update_screen() {
-  Log("更新");
   update_screen();
   vgactl_port_base[1] = 0;
   // TODO: call `update_screen()` when the sync register is non-zero,
@@ -67,6 +66,7 @@ static void vga_ctl_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset == 0 || offset == 4);
   if (is_write && offset == 4) {
     if (vgactl_port_base[1] & 0x1){
+      Log("更新");
       vga_update_screen();
     }
   }
