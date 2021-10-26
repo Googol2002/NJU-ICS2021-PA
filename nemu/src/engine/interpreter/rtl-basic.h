734f79563004bc921ac7fbdb173db149401a66a1
tracer-ics2021 <tracer@njuics.org>
2021-10-26 15:25:04 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: e1c25f9f23d74714af87d8e3d7ab98a8     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  15:25:04 up  1:20,  1 user,  load average: 0.33, 0.14, 0.14

--- nemu/src/engine/interpreter/rtl-basic.h
+++ nemu/src/engine/interpreter/rtl-basic.h
@@ -7,7 +7,7 @@
 /* RTL basic instructions */
 
 #define def_rtl_compute_reg(name) \
-  static inline def_rtl(name, rtlreg_t* dest, const rtlreg_t* src1, const rtlreg_t* src2) { \
+  inline def_rtl(name, rtlreg_t* dest, const rtlreg_t* src1, const rtlreg_t* src2) { \
     *dest = concat(c_, name) (*src1, *src2); \
   }
 
