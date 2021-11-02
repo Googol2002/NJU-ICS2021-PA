5df5f42b24870c21809a2f381925587f74788824
tracer-ics2021 <tracer@njuics.org>
2021-11-02 21:30:58 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: d9a7b506cf5d4ffb9fc7e63ede3b073a     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  21:30:59 up  5:17,  2 users,  load average: 1.43, 1.34, 1.23

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -5,5 +5,11 @@ NEMUState nemu_state = { .state = NEMU_STOP };
 int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
-  return !good;
+  if (good){
+    return 0;
+  }else {
+    return nemu_state.state;
+  }
+  
+  //return !good;
 }
