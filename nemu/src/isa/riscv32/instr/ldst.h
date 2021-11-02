ce662b4d6d2977610deee6d15b2b242efdd71da4
tracer-ics2021 <tracer@njuics.org>
2021-11-02 15:44:01 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: d9a7b506cf5d4ffb9fc7e63ede3b073a     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  15:44:01 up 40 min,  2 users,  load average: 0.02, 0.22, 0.27

--- nemu/src/isa/riscv32/instr/ldst.h
+++ nemu/src/isa/riscv32/instr/ldst.h
@@ -14,6 +14,10 @@ def_EHelper(lhu) {
   rtl_lm(s, ddest, dsrc1, id_src2->imm, 2);
 }
 
+def_EHelper(lb) {
+  rtl_lms(s, ddest, dsrc1, id_src2->imm, 1);
+}
+
 def_EHelper(sw) {
   rtl_sm(s, ddest, dsrc1, id_src2->imm, 4);
 }
