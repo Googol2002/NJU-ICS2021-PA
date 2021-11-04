2956d11b2c2d7bc5d8a5941bda3f904f728f62e7
tracer-ics2021 <tracer@njuics.org>
2021-11-04 14:11:04 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 15e9808196024ddb9c4ab63b2001c539     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  14:11:05 up 38 min,  2 users,  load average: 0.20, 0.24, 0.17

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -13,25 +13,25 @@ int main() {
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
 
-  init_mm();
+//   init_mm();
 
-  init_device();
+//   init_device();
 
-  init_ramdisk();
+//   init_ramdisk();
 
-#ifdef HAS_CTE
-  init_irq();
-#endif
+// #ifdef HAS_CTE
+//   init_irq();
+// #endif
 
-  init_fs();
+//   init_fs();
 
-  init_proc();
+//   init_proc();
 
-  Log("Finish initialization");
+//   Log("Finish initialization");
 
-#ifdef HAS_CTE
-  yield();
-#endif
+// #ifdef HAS_CTE
+//   yield();
+// #endif
 
-  panic("Should not reach here");
+//   panic("Should not reach here");
 }
