6f9ca6ad9efda1773d3aaed2aa1794c88b4ac99b
tracer-ics2021 <tracer@njuics.org>
2021-11-05 23:09:05 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a8186582d8754f02afec51abfe9a1c44     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  23:09:05 up  2:13,  2 users,  load average: 0.72, 0.45, 0.31

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,7 +7,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    printf("%d %d %d", c->mcause, c->mstatus, c->mepc);
+    printf("%d %d %d \n", c->mcause, c->mstatus, c->mepc);
     switch (c->mcause) {
       default: ev.event = EVENT_ERROR; break;
     }
