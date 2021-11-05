d49ef57959d7d7dd40ae51cb73b5309506766d78
tracer-ics2021 <tracer@njuics.org>
2021-11-05 23:23:44 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a8186582d8754f02afec51abfe9a1c44     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  23:23:44 up  2:28,  2 users,  load average: 0.18, 0.17, 0.24

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -68,7 +68,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
         break;
 
       case 'p':
-        pointer = va_arg(ap, uint64_t);
+        pointer = va_arg(ap, uint32_t);
         for (len = 0; pointer ; pointer /= 16, ++len)
           buffer[len] = HEX_CHARACTERS[pointer % 16];//逆序的
         for (int k = 0; k < BIT_WIDE_HEX - len; ++k)
