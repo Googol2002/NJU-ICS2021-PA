64182bcab7e38aad44f440ea3c5ca7d5ed3ec83c
tracer-ics2021 <tracer@njuics.org>
2021-11-11 19:38:56 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 033fb43173cc43a29df912006de0d7d9     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  19:38:56 up  1:52,  1 user,  load average: 0.25, 0.08, 0.09

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,12 +1,19 @@
 #include <common.h>
 #include "syscall.h"
 
+void sys_yield(Context *c){
+  yield();
+}
+
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
-
+    case SYS_yield:
+      sys_yield(c);
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
