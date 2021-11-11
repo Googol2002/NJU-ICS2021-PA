976c5408afd90969827ec0c7c9547a0107759dac
tracer-ics2021 <tracer@njuics.org>
2021-11-11 20:05:32 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 033fb43173cc43a29df912006de0d7d9     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:05:32 up  2:18,  2 users,  load average: 0.05, 0.05, 0.06

--- abstract-machine/am/include/arch/riscv32-nemu.h
+++ abstract-machine/am/include/arch/riscv32-nemu.h
@@ -8,10 +8,11 @@ struct Context {
   void *pdir;
 };
 
+// "a7", "a0", "a1", "a2", "a0"
 #define GPR1 gpr[17] // a7
-#define GPR2 gpr[0]
-#define GPR3 gpr[0]
-#define GPR4 gpr[0]
-#define GPRx gpr[0]
+#define GPR2 gpr[10] //a0
+#define GPR3 gpr[11] //a1
+#define GPR4 gpr[12] //a2
+#define GPRx gpr[10] //a0
 
 #endif
