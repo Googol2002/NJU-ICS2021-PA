1cd4f74e48012b25b085ecbfb999720ada46b2fe
tracer-ics2021 <tracer@njuics.org>
2021-11-15 22:55:46 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 8cc02da23d21452397f13dec12fc5175     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  22:55:46 up  3:28,  1 user,  load average: 0.20, 0.17, 0.19

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -97,7 +97,7 @@ off_t _lseek(int fd, off_t offset, int whence) {
 int _gettimeofday(struct timeval *tv, struct timezone *tz) {
   tv->tv_usec = 1L;
   int ret = _syscall_(SYS_gettimeofday, (intptr_t)tv, (intptr_t)tz, 0);
-  printf("%p %x\n", tv, tv->tv_usec);
+  printf("0x%p %x\n", tv, tv->tv_usec);
   assert(tv->tv_usec != 1L);
   return ret;
 }
