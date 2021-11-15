a5473b6bc334bb812e5560061eded8d6ff7da0f0
tracer-ics2021 <tracer@njuics.org>
2021-11-15 21:04:32 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 8cc02da23d21452397f13dec12fc5175     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  21:04:32 up  2:30,  1 user,  load average: 0.78, 0.37, 0.20

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -10,7 +10,9 @@ int main(){
   struct timeval tv;
   while (1) {
     gettimeofday(&tv, NULL);
-    while(tv.tv_usec / 1000000 < sec) {};
+    while(tv.tv_usec / 1000000 < sec) {
+      gettimeofday(&tv, NULL);
+    };
     //rtc = io_read(AM_TIMER_RTC);
     //printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
     
