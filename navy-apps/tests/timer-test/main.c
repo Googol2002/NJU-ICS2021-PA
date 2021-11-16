c9b237b31153bf7ae10581c118eb1c35e791c510
tracer-ics2021 <tracer@njuics.org>
2021-11-16 20:49:48 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a7d3da67303b430e9f9d664d5598b883     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:49:48 up  1:17,  1 user,  load average: 0.03, 0.04, 0.12

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -2,32 +2,23 @@
 #include <assert.h>
 #include <sys/time.h>
 #include <stdint.h>
-
-int NDL_Init(uint32_t flags);
-void NDL_Quit();
-uint32_t NDL_GetTicks();
+#include <NDL.h>
 
 int main(){
   NDL_Init(0);
 
-  __uint64_t time;
-  __uint64_t usec = 500000;
-  struct timeval tv;
+  uint32_t time;
+  uint32_t msec = 500;
   printf("Hello. %d\n", sizeof(struct timeval));
   while (1) {
-    gettimeofday(&tv, NULL);
-    time = tv.tv_sec * 1000000 + tv.tv_usec;
-    while(time < usec) {
-      gettimeofday(&tv, NULL);
-      time = tv.tv_sec * 1000000 + tv.tv_usec;
+    time = NDL_GetTicks();
+    while(time < msec) {
+      time = NDL_GetTicks();
     };
-    //rtc = io_read(AM_TIMER_RTC);
-    //printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
     
     printf("Oh, 0.5s have been lost...\n");
-    usec += 500000;
+    msec += 500;
   }
 
-  
-  //NDL_Quit();
+  NDL_Quit();
 }
