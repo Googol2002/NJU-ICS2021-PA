72424644709cd9737bf5ab2a5dd8d267b9d3672b
tracer-ics2021 <tracer@njuics.org>
2021-11-16 21:22:16 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a7d3da67303b430e9f9d664d5598b883     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  21:22:16 up  1:50,  2 users,  load average: 0.51, 0.29, 0.12

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -37,7 +37,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   }
   real_length += strlen(keyname[ev.keycode]);
   if (real_length <= len){
-    strcat(buf, tag);
+    strcat(buf, keyname[ev.keycode]);
   }else {
     return 0;
   }
