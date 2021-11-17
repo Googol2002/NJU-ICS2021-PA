cea1a9163accd96b4ceaafcc9ce6acd02c56b6a9
tracer-ics2021 <tracer@njuics.org>
2021-11-17 16:42:16 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a7d3da67303b430e9f9d664d5598b883     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  16:42:16 up  2:29,  3 users,  load average: 1.34, 1.25, 1.20

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -2,9 +2,17 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+const char *regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for (int i = 0; i < 32; ++i){
     if (ref_r->gpr[i]._32 != cpu.gpr[i]._32)
+      Log("对于[%s]寄存器, expcet %x, but got %x.", regs[i], ref_r->gpr[i]._32, cpu.gpr[i]._32);
       return false;
   }
 
