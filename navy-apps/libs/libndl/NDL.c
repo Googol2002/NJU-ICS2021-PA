19e2177fba54f893553655471ed3cc46e5ed76c0
tracer-ics2021 <tracer@njuics.org>
2021-11-17 19:51:44 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a7d3da67303b430e9f9d664d5598b883     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  19:51:44 up  4:31,  3 users,  load average: 1.08, 1.03, 0.92

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -76,7 +76,7 @@ int NDL_Init(uint32_t flags) {
    /* 继续获取其他的子字符串 */
    while( token != NULL ) {
       printf("%s\n", token);
-      sscanf(token, "%s:%d", key, &value);
+      sscanf(token, "%[a-zA-Z]:%d", key, &value);
 
       if(strcmp(key, "WIDTH") == 0){
         screen_w = value;
