837a4e581ea484d77d0d617ea15356775c8335a8
tracer-ics2021 <tracer@njuics.org>
2021-11-17 23:05:27 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a7d3da67303b430e9f9d664d5598b883     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  23:05:27 up  6:57,  2 users,  load average: 0.05, 0.26, 0.63

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -77,8 +77,8 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   //TODO: 奇怪的操作
   Log("%d\n", offset);
-  const uint32_t *src = (uint32_t *)buf;
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR; //字节编址
+  const uint8_t *src = (uint8_t *)buf;
+  uint8_t *fb = (uint8_t *)(uintptr_t)FB_ADDR; //字节编址
 
   for (int i = 0; i < len; ++i){
     fb[offset + i] = src[i];
