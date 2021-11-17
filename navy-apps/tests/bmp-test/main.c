3665799ef0de5de68af7029276936eab73e103d2
tracer-ics2021 <tracer@njuics.org>
2021-11-17 22:53:46 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a7d3da67303b430e9f9d664d5598b883     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  22:53:46 up  6:45,  2 users,  load average: 0.93, 1.07, 1.10

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -3,12 +3,17 @@
 #include <stdlib.h>
 #include <NDL.h>
 #include <BMP.h>
+#include <string.h>
 
 int main() {
   printf("Doing bmp-test...\n");
   NDL_Init(0);
   int w, h;
-  void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  //void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  w = 128;
+  h = 128;
+  void *bmp = malloc(w * h * sizeof(uint32_t));
+  memset(bmp, 0x00FF0000, w * h);
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
   NDL_DrawRect(bmp, 0, 0, w, h);
