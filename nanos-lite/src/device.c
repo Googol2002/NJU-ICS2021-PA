2e194e8b6ff2f2dccc1385b53e2cf95759cbb23a
tracer-ics2021 <tracer@njuics.org>
2021-11-17 20:00:34 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a7d3da67303b430e9f9d664d5598b883     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:00:34 up  4:40,  3 users,  load average: 1.12, 0.97, 0.93

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -60,7 +60,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   int w = io_read(AM_GPU_CONFIG).width;
   int h = io_read(AM_GPU_CONFIG).height;
 
-  int ret = snprintf(buf, len, "WIDTH :%d\nHEIGHT : %d", w, h);
+  int ret = snprintf(buf, len, "WIDTH:%d\nHEIGHT : %d", w, h);
 
   if (ret >= len){
     assert(0);
