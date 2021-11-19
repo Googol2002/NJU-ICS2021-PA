6308927f7888993748fbeff3c0c6d69f496c11b4
tracer-ics2021 <tracer@njuics.org>
2021-11-19 15:53:10 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 34b50862828f493bb88de486077443ac     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  15:53:10 up  7:19,  2 users,  load average: 0.17, 0.36, 0.63

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -53,6 +53,10 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   //Log("I'm reading");
 
+  if (offset > 0){
+    return 0;
+  }
+
   int w = io_read(AM_GPU_CONFIG).width;
   int h = io_read(AM_GPU_CONFIG).height;
 
