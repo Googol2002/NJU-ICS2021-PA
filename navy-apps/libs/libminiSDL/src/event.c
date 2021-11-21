02f0fea5da11a7dc8d02ee14b0a8d28955d38619
tracer-ics2021 <tracer@njuics.org>
2021-11-21 14:35:04 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: b50c249ba7be4f4dbc3103e549f6776f     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  14:35:04 up  6:33,  2 users,  load average: 0.53, 0.78, 0.62

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -11,11 +11,12 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
-// typedef struct sdl_event_element{
-//   uint8_t type;
-//   uint8_t sym;
-//   struct sdl_event_element* next;
-// } sdl_event_element;
+struct __event_element{
+  uint8_t type;
+  uint8_t sym;
+  struct __event_element* next;
+};
+typedef struct __event_element event_element;
 
 // sdl_event_element event_queue = {.type = 0, .sym = 0, .next = NULL};
 // sdl_event_element *end = &event_queue;
