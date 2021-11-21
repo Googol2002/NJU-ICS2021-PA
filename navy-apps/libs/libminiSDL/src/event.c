09183f4f12db6e3a2c63fc415208b5bbe55e442a
tracer-ics2021 <tracer@njuics.org>
2021-11-21 14:29:16 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: b50c249ba7be4f4dbc3103e549f6776f     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  14:29:16 up  6:27,  2 users,  load average: 0.53, 0.71, 0.50

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -2,6 +2,7 @@
 #include <SDL.h>
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>
 
 #define keyname(k) #k,
 
@@ -9,6 +10,39 @@ static const char *keyname[] = {
   "NONE",
   _KEYS(keyname)
 };
+
+typedef struct event_element{
+  uint8_t type;
+  uint8_t sym;
+  struct event_element* next;
+} event_element;
+
+event_element event_queue = {.type = 0, .sym = 0, .next = NULL};
+event_element *end = &event_queue;
+
+static void append(uint8_t type, uint8_t sym){
+  event_element *new_element = malloc(sizeof(event_element));
+  new_element->type = type;
+  new_element->sym = sym;
+  new_element->next = NULL;
+  end->next = new_element;
+  end = new_element;
+}
+
+static int pop(uint8_t *type, uint8_t *sym){
+  if (event_queue.next == NULL){
+    return 0;
+  }else {
+    event_element *buf = event_queue.next;
+    *type = buf->type;
+    *sym = buf->sym;
+    event_queue.next = buf->next;
+    free (buf);
+  }
+  return 1;
+}
+
+static uint8_t key_state[sizeof(keyname) / sizeof(keyname[0])] = {0};
 
 int SDL_PushEvent(SDL_Event *ev) {
   return 0;
