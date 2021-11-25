8d6753b44344971b9666bfc440ecf2f59d5d8a47
tracer-ics2021 <tracer@njuics.org>
2021-11-25 04:11:38 -0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: aa5f64e7b7ac4e75b4a94220feae126b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  04:11:38 up  3:45,  1 user,  load average: 0.97, 1.22, 1.00

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -19,7 +19,5 @@ uint32_t SDL_GetTicks() {
 
 void SDL_Delay(uint32_t ms) {
   uint32_t start = NDL_GetTicks();
-  while (NDL_GetTicks() - start <= ms){
-    printf("%d %d\n", NDL_GetTicks() - start, ms);
-  }
+  while (NDL_GetTicks() - start <= ms){}
 }
