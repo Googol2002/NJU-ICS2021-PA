634f855b0f6f87e17c94a81f8b2cc24000fe48db
tracer-ics2021 <tracer@njuics.org>
2021-11-26 17:36:42 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 20187e80d7474797b44ce361512d2865     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  17:36:42 up  2:02,  1 user,  load average: 0.94, 1.13, 1.05

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -98,7 +98,7 @@ static int inline read_keyinfo(uint8_t *type, uint8_t *sym){
   key_key = &key_buf[i + 1]; 
   
   //截断\n
-  for (i = 0; key_key[i] != '\n' && key_key[i] != '\0'; i++){}
+  for (i = 0;  key_key[i] != '\0' && key_key[i] != '\n'; i++){}
   if (key_key[i] == '\n'){
     key_key[i] = '\0';
   }
@@ -115,7 +115,7 @@ static int inline read_keyinfo(uint8_t *type, uint8_t *sym){
     //剪枝掉很多
     if (key_key[0] == keyname[i][0] && strcmp(key_key, keyname[i]) == 0){
       *sym = i;
-      printf("%d %d\n", *type, *sym);
+      //printf("%d %d\n", *type, *sym);
       return ret;
     }
   }
