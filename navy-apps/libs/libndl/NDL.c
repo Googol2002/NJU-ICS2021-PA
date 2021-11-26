44990c4d6f01d68e97df1471f8149bf473a51541
tracer-ics2021 <tracer@njuics.org>
2021-11-26 17:52:31 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 20187e80d7474797b44ce361512d2865     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  17:52:32 up  2:18,  1 user,  load average: 0.83, 1.08, 1.17

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -67,12 +67,13 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   // }
   // fclose(graphics);
   int graphics = open("/dev/fb", O_RDWR);
+  printf("NDL_Draw\n");
   
   for (int i = 0; i < h; ++i){
     lseek(graphics, ((canvas_y + y + i) * screen_w + (canvas_x + x)) * sizeof(uint32_t), SEEK_SET);
     ssize_t s = write(graphics, pixels + w * i, w * sizeof(uint32_t));
   }
-  close(graphics);
+  //close(graphics);
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
