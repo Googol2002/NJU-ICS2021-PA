48fb77027b5c2e5d25a9b3e234694a7cca1d6f15
tracer-ics2021 <tracer@njuics.org>
2021-12-04 16:26:58 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 30364f6cdec54d3597248c96ca3c1639     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  16:26:58 up  2:26,  2 users,  load average: 0.13, 0.16, 0.35

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg);
 
 void init_proc() {
   context_kload(&pcb[0], hello_fun, "ONE");
-  context_kload(&pcb[0], hello_fun, "TWO");
+  context_kload(&pcb[1], hello_fun, "TWO");
 
   switch_boot_pcb();
   // Log("Initializing processes...");
