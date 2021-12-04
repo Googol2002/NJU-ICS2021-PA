e7a8d71b93b9ad55611104f849707a62751ccefe
tracer-ics2021 <tracer@njuics.org>
2021-12-04 15:41:39 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 30364f6cdec54d3597248c96ca3c1639     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:41:39 up  1:40,  2 users,  load average: 0.68, 0.43, 0.25

--- nemu/src/isa/riscv32/instr/spec.h
+++ nemu/src/isa/riscv32/instr/spec.h
@@ -35,7 +35,7 @@ def_EHelper(csrrw) {
 def_EHelper(ecall) {
   IFDEF(CONFIG_ETRACE, Log("etrace: ecall"));
   //Log("PRIV: %d", s->isa.instr.i.funct3);
-  isa_raise_intr(User_Software_Interrupt, s->pc);
+  isa_raise_intr(Machine_Software_Interrupt, s->pc);
   rtl_mv(s, &s->dnpc, &(csr.mtvec));
 }
 
