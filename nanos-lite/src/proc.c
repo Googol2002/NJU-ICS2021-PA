5bae039b006a6e6bb5aef723eb224c3ec3fe273f
tracer-ics2021 <tracer@njuics.org>
2021-12-05 21:04:57 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: bb5711fb45354fa6a828ee5572468bcb     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  21:04:57 up  3:37,  1 user,  load average: 0.25, 0.26, 0.18

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -50,7 +50,7 @@ Context* schedule(Context *prev) {
 
 int execve(const char *filename, char *const argv[], char *const envp[]){
   context_uload((current == &pcb[0] ? &pcb[1] : &pcb[0]), filename, argv, envp);
-  // printf("execve Loaded\n");
+  printf("execve Loaded %x\n", (current == &pcb[0] ? &pcb[1] : &pcb[0])->cp->mepc);
   switch_boot_pcb();
   yield();
   return 0;
