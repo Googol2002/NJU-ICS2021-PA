d23c1cb3e0196c2cb1a6f8b3bd637edebeb5034c
tracer-ics2021 <tracer@njuics.org>
2021-12-05 17:33:29 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: bb5711fb45354fa6a828ee5572468bcb     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  17:33:29 up  1:23,  1 user,  load average: 0.24, 0.42, 0.30

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,15 +5,8 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  printf("I'm OK!\n");
-  uintptr_t argc = *args;
-  char **argv = (char **)(args + 1);
-
-
-  for (args += 1; args; ++args){}
-  char **envp = (char **)(args + 1);
-
-  environ = envp;
-  exit(main(argc, argv, envp));
+  char *empty[] =  {NULL };
+  environ = empty;
+  exit(main(0, empty, empty));
   assert(0);
 }
