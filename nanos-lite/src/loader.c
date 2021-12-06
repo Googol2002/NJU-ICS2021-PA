6c7aa9392ce412d6e3cb230321d3f758de96f0bd
tracer-ics2021 <tracer@njuics.org>
2021-12-06 10:26:45 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: bb5711fb45354fa6a828ee5572468bcb     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  10:26:45 up  5:57,  1 user,  load average: 0.14, 0.46, 0.44

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -86,6 +86,12 @@ static size_t ceil_4_bytes(size_t size){
 #define NR_PAGE 8
 
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]){
+  
+  if (envp){
+    for (int i = 0; envp[i]; ++i){
+      printf("env:%x\n", envp[i]);
+    }
+  }
   uintptr_t entry = loader(pcb, filename);
 
   Area karea;
@@ -105,11 +111,6 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   if (argv){
     for (int i = 0; argv[i]; ++i){
       printf("arg:%s\n", argv[i]);
-    }
-  }
-  if (envp){
-    for (int i = 0; envp[i]; ++i){
-      printf("env:%x\n", envp[i]);
     }
   }
   printf("I'm OK, %d, %x\n", argc, envp);
