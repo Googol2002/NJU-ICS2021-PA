79d4e1ed51f5fe9e5ba52175c20b05e324fec9e2
tracer-ics2021 <tracer@njuics.org>
2021-12-09 22:58:08 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a9b38e5e38134b3eafcdaff745a84a7e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  22:58:08 up  2:54,  1 user,  load average: 0.05, 0.10, 0.10

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -31,6 +31,7 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
 
   kas.ptr = pgalloc_f(PGSIZE);
 
+  printf("I'm here\n");
   int i;
   for (i = 0; i < LENGTH(segments); i ++) {
     void *va = segments[i].start;
