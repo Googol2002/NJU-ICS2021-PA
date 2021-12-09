f3cb19e10dc6752631f81dc004d8ce9f7335f433
tracer-ics2021 <tracer@njuics.org>
2021-12-09 23:18:59 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a9b38e5e38134b3eafcdaff745a84a7e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:18:59 up  3:15,  1 user,  load average: 0.14, 0.15, 0.10

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -4,9 +4,12 @@
 
 extern const char *regs[];
 
+void error_finfo();
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for (int i = 0; i < 32; ++i){
     if (ref_r->gpr[i]._32 != cpu.gpr[i]._32){
+      error_finfo();
       Log("对于[%s]寄存器, expcet %x, but got %x.", regs[i], ref_r->gpr[i]._32, cpu.gpr[i]._32);
       return false;
     }
