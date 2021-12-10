afcbefc31f0e2224b9b4dabb704a95b0cbd7fa9f
tracer-ics2021 <tracer@njuics.org>
2021-12-10 14:33:58 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a9b38e5e38134b3eafcdaff745a84a7e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:33:58 up  5:35,  1 user,  load average: 0.03, 0.15, 0.17

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -78,6 +78,7 @@ void __am_switch(Context *c) {
 
 void map(AddrSpace *as, void *va, void *pa, int prot) {
   PTE *page_table_entry = as->ptr + VA_VPN_1(va) * 4;
+  printf("%p\t%p\n", as->ptr, get_satp() + VA_VPN_1(va) * 4);
   assert((uintptr_t)as->ptr == get_satp() + VA_VPN_1(va) * 4);
 
   if (!(*page_table_entry & PTE_V)){ // 说明二级表未分配
