f10b8cf0ed0b723337315f5dc3e9421adc90e4a9
tracer-ics2021 <tracer@njuics.org>
2021-12-11 15:17:59 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a9b38e5e38134b3eafcdaff745a84a7e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:17:59 up 14:50,  1 user,  load average: 0.20, 0.12, 0.09

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -19,14 +19,14 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   paddr_t page_table_entry_addr = (csr.satp << 12) + VA_VPN_1(vaddr) * 4;
   PTE page_table_entry = paddr_read(page_table_entry_addr, 4);
   if ((page_table_entry & PTE_V) == 0){
-    printf("page_table_entry not valid, vaddr: %#x", vaddr);
+    Log("page_table_entry not valid, vaddr: %#x", vaddr);
   }
   assert(page_table_entry & PTE_V);
 
   paddr_t leaf_page_table_entry_addr = PTE_PPN(page_table_entry) * 4096 + VA_VPN_0(vaddr) * 4;
   PTE leaf_page_table_entry = paddr_read(leaf_page_table_entry_addr, 4);
-  if (!(leaf_page_table_entry & PTE_V)){
-    printf("leaf_page_table_entry not valid, vaddr: %#x", vaddr);
+  if ((leaf_page_table_entry & PTE_V) == 0){
+    Log("leaf_page_table_entry not valid, vaddr: %#x", vaddr);
   }
   assert(leaf_page_table_entry & PTE_V);
 
