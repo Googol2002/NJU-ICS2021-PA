5165337bbab33955b6d3b1c198ad38bcd70af5f8
tracer-ics2021 <tracer@njuics.org>
2021-12-11 15:49:07 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a9b38e5e38134b3eafcdaff745a84a7e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:49:07 up 15:21,  1 user,  load average: 0.22, 0.15, 0.10

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -135,7 +135,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   
   intptr_t *ptr_brk = (intptr_t *)(brk);
   
-  printf("Loading... %x\n", ptr_brk);
+
   // 分配envp空间
   ptr_brk -= 1;
   *ptr_brk = 0;
@@ -143,6 +143,8 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   for (int i = 0; i < envc; ++i){
     ptr_brk[i] = (intptr_t)(envp_ustack[i]);
   }
+
+  printf("Loading... %x\n", ptr_brk);
 
   // 分配argv空间
   ptr_brk -= 1;
