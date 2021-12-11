e6d21e7d53b00879e09a987d020c421e59a1fa9a
tracer-ics2021 <tracer@njuics.org>
2021-12-11 15:40:21 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a9b38e5e38134b3eafcdaff745a84a7e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:40:21 up 15:13,  1 user,  load average: 0.20, 0.17, 0.10

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -141,6 +141,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     ptr_brk[i] = (intptr_t)(envp_ustack[i]);
   }
 
+  printf("Loading...\n");
   // 分配argv空间
   ptr_brk -= 1;
   *ptr_brk = 0;
@@ -152,7 +153,6 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   ptr_brk -= 1;
   *ptr_brk = argc;
   
-  printf("Loading...\n");
   //这条操作会把参数的内存空间扬了，要放在最后
   uintptr_t entry = loader(pcb, filename);
   Area karea;
