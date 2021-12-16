664e2d50c09cdb448daa6f864ea8874fa1ca3a0c
tracer-ics2021 <tracer@njuics.org>
2021-12-16 18:51:22 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 4cd40d6d3204407ab25f17633bf72e25     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  18:51:22 up 15 min,  1 user,  load average: 0.12, 0.27, 0.38

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -28,6 +28,19 @@ static void read(int fd, void *buf, size_t offset, size_t len){
 #define NR_PAGE 8
 #define PAGESIZE 4096
 
+static void * alloc_section_space(AddrSpace *as, uintptr_t vaddr, size_t p_memsz){
+  size_t page_n = p_memsz % PAGESIZE == 0 ? p_memsz / 4096 : (p_memsz / 4096 + 1);
+  void *page_end = new_page(page_n);
+  void *page_start = page_end - page_n * PAGESIZE;
+
+  for (int i = 0; i < page_n; ++i){
+    // TODO: 这里prot参数不规范
+    map(as, (void *)(vaddr + i * PAGESIZE), (void *)(page_start + i * PAGESIZE), 0);
+  }
+
+  return page_start;
+}
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
   int fd = fs_open(filename, 0, 0);
   if (fd == -1){ 
@@ -37,16 +50,16 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   AddrSpace *as = &pcb->as;
   // TODO: 这里prot参数不规范
   // as->area.start 0x40000000
-  void *alloced_page = new_page(8);
-  void *alloced_page_start = alloced_page - PAGESIZE * 8;
-  map(as, as->area.start, alloced_page_start, 0);
-  map(as, as->area.start + 1 * PAGESIZE, alloced_page_start + 1 * PAGESIZE, 0);
-  map(as, as->area.start + 2 * PAGESIZE, alloced_page_start + 2 * PAGESIZE, 0);
-  map(as, as->area.start + 3 * PAGESIZE, alloced_page_start + 3 * PAGESIZE, 0);
-  map(as, as->area.start + 4 * PAGESIZE, alloced_page_start + 4 * PAGESIZE, 0);
-  map(as, as->area.start + 5 * PAGESIZE, alloced_page_start + 5 * PAGESIZE, 0);
-  map(as, as->area.start + 6 * PAGESIZE, alloced_page_start + 6 * PAGESIZE, 0);
-  map(as, as->area.start + 7 * PAGESIZE, alloced_page_start + 7 * PAGESIZE, 0);
+  // void *alloced_page = new_page(8);
+  // void *alloced_page_start = alloced_page - PAGESIZE * 8;
+  // map(as, as->area.start, alloced_page_start, 0);
+  // map(as, as->area.start + 1 * PAGESIZE, alloced_page_start + 1 * PAGESIZE, 0);
+  // map(as, as->area.start + 2 * PAGESIZE, alloced_page_start + 2 * PAGESIZE, 0);
+  // map(as, as->area.start + 3 * PAGESIZE, alloced_page_start + 3 * PAGESIZE, 0);
+  // map(as, as->area.start + 4 * PAGESIZE, alloced_page_start + 4 * PAGESIZE, 0);
+  // map(as, as->area.start + 5 * PAGESIZE, alloced_page_start + 5 * PAGESIZE, 0);
+  // map(as, as->area.start + 6 * PAGESIZE, alloced_page_start + 6 * PAGESIZE, 0);
+  // map(as, as->area.start + 7 * PAGESIZE, alloced_page_start + 7 * PAGESIZE, 0);
   
   Elf_Ehdr elf_header;
   read(fd, &elf_header, 0, sizeof(elf_header));
@@ -61,14 +74,18 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     Elf_Phdr section_entry;
     read(fd, &section_entry, 
       i * headers_entry_size + program_header_offset, sizeof(elf_header));
-    void * virt_addr;
+    void *phys_addr;
+    uintptr_t virt_addr;
     switch (section_entry.p_type) {
     case PT_LOAD:
       // virt_addr = (void *)section_entry.p_vaddr; 
-      virt_addr = (void *)alloced_page_start + (section_entry.p_vaddr - 0x40000000); // 这里是把0x40000000加载到他对应的实际地址
-      printf("Load to %x with %x\n", virt_addr, section_entry.p_vaddr);
-      read(fd, virt_addr, section_entry.p_offset, section_entry.p_filesz);
-      memset(virt_addr + section_entry.p_filesz, 0, 
+      // virt_addr = (void *)alloced_page_start + (section_entry.p_vaddr - 0x40000000); // 这里是把0x40000000加载到他对应的实际地址
+      virt_addr = section_entry.p_vaddr;
+      phys_addr = alloc_section_space(as, virt_addr, section_entry.p_memsz);
+
+      printf("Load to %x with %x\n", phys_addr, section_entry.p_vaddr);
+      read(fd, phys_addr, section_entry.p_offset, section_entry.p_filesz);
+      memset(phys_addr + section_entry.p_filesz, 0, 
         section_entry.p_memsz - section_entry.p_filesz);
       break;
     
