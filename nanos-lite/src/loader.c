16409b67166e9c1ac220211107164beb23179be6
tracer-ics2021 <tracer@njuics.org>
2021-12-16 23:07:33 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 4cd40d6d3204407ab25f17633bf72e25     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:07:33 up  3:12,  1 user,  load average: 0.18, 0.12, 0.08

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -78,9 +78,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       memset(phys_addr + section_entry.p_filesz, 0, 
         section_entry.p_memsz - section_entry.p_filesz);
       
-      if (section_entry.p_filesz < section_entry.p_memsz){// 应该是.bss节
-        current->max_brk = section_entry.p_vaddr + section_entry.p_memsz;
-      }
+      // if (section_entry.p_filesz < section_entry.p_memsz){// 应该是.bss节
+      //   current->max_brk = section_entry.p_vaddr + section_entry.p_memsz;
+      // }
       
       break;
     
