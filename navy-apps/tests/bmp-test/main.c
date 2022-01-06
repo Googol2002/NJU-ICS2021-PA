25115a6ab8ae6213326082bb01dba36cacf965bf
tracer-ics2021 <tracer@njuics.org>
2022-01-06 14:57:55 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: cfec42e339ad4f0d877fa75b5fdc0467     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:57:55 up  3:15,  3 users,  load average: 0.24, 0.20, 0.13

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
-#include <NDL.h>
-#include <BMP.h>
+#include <SDL.h>
+#include <SDL_bmp.h>
 #include <string.h>
 
 int main() {
-  // printf("Doing bmp-test...\n");
-  NDL_Init(0);
+  SDL_Init(0);
+  SDL_Surface *screen = SDL_SetVideoMode(400, 300, 32, SDL_HWSURFACE);
   int w, h;
-  void *bmp = BMP_Load("/share/pictures/test.bmp", &w, &h);
-  // assert(bmp);
-  // NDL_OpenCanvas(&w, &h);
-  // NDL_DrawRect(bmp, 0, 0, w, h);
-  // free(bmp);
-  // NDL_Quit();
-  // printf("Test ends! Spinning...\n");
-  // while (1);
+  SDL_Surface *slide = SDL_LoadBMP("/share/pictures/test.bmp");
+
+  assert(slide);
+  SDL_UpdateRect(slide, 0, 0, 0, 0);
+
+  SDL_Quit();
+  printf("Test ends! Spinning...\n");
+  while (1);
   return 0;
 }
