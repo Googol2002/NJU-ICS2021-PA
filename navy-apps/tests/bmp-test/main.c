b974b7bbc6961079869ea9fcc9106f65a82d68b4
tracer-ics2021 <tracer@njuics.org>
2022-01-06 14:20:14 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: cfec42e339ad4f0d877fa75b5fdc0467     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:20:14 up  2:37,  3 users,  load average: 0.37, 0.25, 0.29

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -7,8 +7,8 @@
 
 int main() {
   printf("Doing bmp-test...\n");
-  // NDL_Init(0);
-  // int w, h;
+  NDL_Init(0);
+  int w, h;
   // void *bmp = BMP_Load("/share/pictures/test.bmp", &w, &h);
   // assert(bmp);
   // NDL_OpenCanvas(&w, &h);
