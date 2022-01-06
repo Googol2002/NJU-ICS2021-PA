dc6911010b6b63bbe59950883e419dfd1598c6d3
tracer-ics2021 <tracer@njuics.org>
2022-01-06 19:07:58 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: cfec42e339ad4f0d877fa75b5fdc0467     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  19:07:58 up  7:25,  3 users,  load average: 0.09, 0.06, 0.01

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -172,9 +172,6 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     ptr_brk[i] = (intptr_t)(envp_ustack[i]);
   }
 
-  printf("SB\n");
-  printf("AAAAAA: %d\n", (intptr_t)ptr_brk - (intptr_t)alloced_page + (intptr_t)as->area.end);
-
   // 分配argv空间
   ptr_brk -= 1;
   *ptr_brk = 0;
@@ -184,10 +181,11 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     ptr_brk[i] = (intptr_t)(argv_ustack[i]);
   }
 
+  printf("SB\n");
   ptr_brk -= 1;
   *ptr_brk = argc;
   
-  
+  printf("AAAAAA: %d\n", (intptr_t)ptr_brk - (intptr_t)alloced_page + (intptr_t)as->area.end);
 
   //这条操作会把参数的内存空间扬了，要放在最后
   uintptr_t entry = loader(pcb, filename);
