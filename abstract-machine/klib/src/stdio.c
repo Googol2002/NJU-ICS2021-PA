b0ac4ac6403a80f755d5dc0b106cd4d74018baac
tracer-ics2021 <tracer@njuics.org>
2022-01-06 14:33:03 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: cfec42e339ad4f0d877fa75b5fdc0467     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:33:03 up  2:50,  3 users,  load average: 0.01, 0.08, 0.17

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -9,16 +9,17 @@ int vsprintf(char *out, const char *fmt, va_list ap);
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap);
 
 int printf(const char *fmt, ...) {
-  char buffer[4096];
-  va_list arg;
-  va_start (arg, fmt);
+  // char buffer[4096];
+  // va_list arg;
+  // va_start (arg, fmt);
   
-  int done = vsprintf(buffer, fmt, arg);
+  // int done = vsprintf(buffer, fmt, arg);
 
-  putstr(buffer);
+  // putstr(buffer);
 
-  va_end(arg);
-  return done;
+  // va_end(arg);
+  // return done;
+  return 1;
 }
 
 static char HEX_CHARACTERS[] = "0123456789ABCDEF";
@@ -26,90 +27,6 @@ static char HEX_CHARACTERS[] = "0123456789ABCDEF";
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   return vsnprintf(out, -1, fmt, ap);
-  // char buffer[128];
-  // char *txt, cha;
-  // int num, len;
-  // unsigned int unum;
-  // uint32_t pointer;
-  
-  
-  // int state = 0, i, j;//模仿一个状态机
-  // for (i = 0, j = 0; fmt[i] != '\0'; ++i){
-  //   switch (state)
-  //   {
-  //   case 0:
-  //     if (fmt[i] != '%')
-  //       out[j++]=fmt[i];
-  //     else
-  //       state = 1;
-  //     break;
-    
-  //   case 1:
-  //     switch (fmt[i])
-  //     {
-  //     case 's':
-  //       txt = va_arg(ap, char*);
-  //       for (int k = 0; txt[k] !='\0'; ++k)
-  //         out[j++] = txt[k];
-  //       break;
-      
-  //     case 'd':
-  //       num = va_arg(ap, int);
-  //       if(num == 0){
-  //         out[j++] = '0';
-  //         break;
-  //       }
-  //       if (num < 0){
-  //         out[j++] = '-';
-  //         num = 0 - num;
-  //       }
-  //       for (len = 0; num ; num /= 10, ++len)
-  //         //buffer[len] = num % 10 + '0';//逆序的
-  //         buffer[len] = HEX_CHARACTERS[num % 10];//逆序的
-  //       for (int k = len - 1; k >= 0; --k)
-  //         out[j++] = buffer[k];
-  //       break;
-      
-  //     case 'c':
-  //       cha = (char)va_arg(ap, int);
-  //       out[j++] = cha;
-  //       break;
-
-  //     case 'p':
-  //       pointer = va_arg(ap, uint32_t);
-  //       for (len = 0; pointer ; pointer /= 16, ++len)
-  //         buffer[len] = HEX_CHARACTERS[pointer % 16];//逆序的
-  //       for (int k = 0; k < BIT_WIDE_HEX - len; ++k)
-  //         out[j++] = '0';
-
-  //       for (int k = len - 1; k >= 0; --k)
-  //         out[j++] = buffer[k];
-  //       break;
-
-  //     case 'x':
-  //       unum = va_arg(ap, unsigned int);
-  //       if(unum == 0){
-  //         out[j++] = '0';
-  //         break;
-  //       }
-  //       for (len = 0; unum ; unum >>= 4, ++len)
-  //         buffer[len] = HEX_CHARACTERS[unum & 0xF];//逆序的
-
-  //       for (int k = len - 1; k >= 0; --k)
-  //         out[j++] = buffer[k];
-  //       break;  
-
-  //     default:
-  //       assert(0);
-  //     }
-  //     state = 0;
-  //     break;
-      
-  //   }
-  // }
-
-  // out[j] = '\0';
-  // return j;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
