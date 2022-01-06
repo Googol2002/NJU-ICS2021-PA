2f6f82d588419cb73ae6ad5689034d70c2e79979
tracer-ics2021 <tracer@njuics.org>
2022-01-06 15:21:04 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: cfec42e339ad4f0d877fa75b5fdc0467     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  15:21:04 up  3:38,  2 users,  load average: 0.52, 0.36, 0.30

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -5,6 +5,22 @@
 #include <BMP.h>
 #include <string.h>
 
+struct BitmapHeader {
+  uint16_t type;
+  uint32_t filesize;
+  uint32_t resv_1;
+  uint32_t offset;
+  uint32_t ih_size;
+  uint32_t width;
+  uint32_t height;
+  uint16_t planes;
+  uint16_t bitcount; // 1, 4, 8, or 24
+  uint32_t compression;
+  uint32_t sizeimg;
+  uint32_t xres, yres;
+  uint32_t clrused, clrimportant;
+} __attribute__((packed));
+
 #ifdef __ISA_NATIVE__
 #error can not support ISA=native
 #endif
@@ -16,8 +32,23 @@ extern char end;
 
 int main() {
   NDL_Init(0);
-  int w, h;
-  void *bmp = BMP_Load("/share/pictures/test.bmp", &w, &h);
+  int w1, h1;
+  const char *filename = "/share/pictures/test.bmp";
+  int *width = &w1; 
+  int *height = &h1;
+
+  FILE *fp = fopen(filename, "r");
+  if (!fp) return NULL;
+
+  struct BitmapHeader hdr;
+  assert(sizeof(hdr) == 54);
+  assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
+
+  if (hdr.bitcount != 24) return NULL;
+  if (hdr.compression != 0) return NULL;
+  int w = hdr.width;
+  int h = hdr.height;
+  uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
 
   printf("Hi, I'm dummy!\n");
   return _syscall_(SYS_yield, 0, 0, 0);
