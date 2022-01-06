669879fff308e53533e3be82b7f3c75af341b36d
tracer-ics2021 <tracer@njuics.org>
2022-01-06 13:55:08 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: cfec42e339ad4f0d877fa75b5fdc0467     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  13:55:08 up  2:12,  2 users,  load average: 1.23, 0.72, 0.65

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -33,17 +33,17 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   int h = hdr.height;
   uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
 
-  int line_off = (w * 3 + 3) & ~0x3;
-  for (int i = 0; i < h; i ++) {
-    fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
-    int nread = fread(&pixels[w * i], 3, w, fp);
-    for (int j = w - 1; j >= 0; j --) {
-      uint8_t b = *(((uint8_t*)&pixels[w * i]) + 3 * j);
-      uint8_t g = *(((uint8_t*)&pixels[w * i]) + 3 * j + 1);
-      uint8_t r = *(((uint8_t*)&pixels[w * i]) + 3 * j + 2);
-      pixels[w * i + j] = (r << 16) | (g << 8) | b;
-    }
-  }
+  // int line_off = (w * 3 + 3) & ~0x3;
+  // for (int i = 0; i < h; i ++) {
+  //   fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
+  //   int nread = fread(&pixels[w * i], 3, w, fp);
+  //   for (int j = w - 1; j >= 0; j --) {
+  //     uint8_t b = *(((uint8_t*)&pixels[w * i]) + 3 * j);
+  //     uint8_t g = *(((uint8_t*)&pixels[w * i]) + 3 * j + 1);
+  //     uint8_t r = *(((uint8_t*)&pixels[w * i]) + 3 * j + 2);
+  //     pixels[w * i + j] = (r << 16) | (g << 8) | b;
+  //   }
+  // }
 
   fclose(fp);
   if (width) *width = w;
