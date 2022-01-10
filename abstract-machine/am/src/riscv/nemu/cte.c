2cfa75ab404804c5cb92db011701baa04e3daf0d
tracer-ics2021 <tracer@njuics.org>
2022-01-10 15:56:12 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 1f4a7edbbfee42fb95545e41e4d985ba     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  15:56:12 up  2:10,  2 users,  load average: 0.09, 0.14, 0.11

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -13,9 +13,8 @@ void __am_get_cur_as(Context *c);
 void __am_switch(Context *c);
 
 Context* __am_irq_handle(Context *c) { 
-  printf("__am_irq_handle c->pdir内容地址修改前 %p %p %p\n", c->pdir, &c->pdir, c);
   __am_get_cur_as(c);
-  
+  printf("__am_irq_handle c->pdir内容地址修改前 %p %p %p\n", c->pdir, &c->pdir, c);
   if (user_handler) {
     Event ev = {0};
     
