85429c7b047d8f886f0e7c876078ee04b1ba865d
tracer-ics2021 <tracer@njuics.org>
2022-01-12 15:13:05 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 66edbcbc457a404eb96ab5fffe52f8d0     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  15:13:05 up  2:56,  2 users,  load average: 0.23, 0.30, 0.33

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -59,11 +59,11 @@ void unprotect(AddrSpace *as) {
 }
 
 void __am_get_cur_as(Context *c) {
-  if (c->pdir != NULL){ //自行添加
-    printf("在__am_get_cur_as中设置为由%p，地址为%p，更改为，", c->pdir, &c->pdir);
+  // if (c->pdir != NULL){ //自行添加
+    // printf("在__am_get_cur_as中设置为由%p，地址为%p，更改为，", c->pdir, &c->pdir);
     c->pdir = (vme_enable ? (void *)get_satp() : NULL);
-    printf("%p\n", c->pdir);
-  }
+    // printf("%p\n", c->pdir);
+  // }
 }
 
 void __am_switch(Context *c) {
