cd3d58f67621554938d9372b99935cd11b43244f
tracer-ics2021 <tracer@njuics.org>
2022-01-13 10:32:33 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a6772e7055ee427baa7d9f400ef121b6     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  10:32:33 up  1:30,  2 users,  load average: 0.09, 0.11, 0.22

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -22,7 +22,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 
 
 word_t isa_query_intr() {
-  if (csr.mstatus.m.MIE && cpu.INTR) {
+  if (csr.mstatus.m.MIE == 1 && cpu.INTR) {
     cpu.INTR = false;
     return IRQ_TIMER;
   }
