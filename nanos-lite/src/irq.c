eace47741853bae014b3d5252e2028ba5070e631
tracer-ics2021 <tracer@njuics.org>
2022-01-13 09:53:21 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a6772e7055ee427baa7d9f400ef121b6     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  09:53:21 up 51 min,  0 users,  load average: 0.56, 0.29, 0.19

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -7,7 +7,6 @@ void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_IRQ_TIMER:
     case EVENT_YIELD:
       // printf("EVENT_YIELD\n");
       return schedule(c); //存起来了现在的，返回了另一个
@@ -16,9 +15,9 @@ static Context* do_event(Event e, Context* c) {
       do_syscall(c);
       break;
 
-    // case EVENT_IRQ_TIMER:
-    //   //Log("EVENT_IRQ_TIMER happening...");
-    //   break;
+    case EVENT_IRQ_TIMER:
+      //Log("EVENT_IRQ_TIMER happening...");
+      break;
 
     default: panic("Unhandled event ID = %d", e.event);
   }
