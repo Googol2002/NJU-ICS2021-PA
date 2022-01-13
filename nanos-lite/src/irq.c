afc5497c67b9e5623e35223ab6b1686777031958
tracer-ics2021 <tracer@njuics.org>
2022-01-13 11:35:55 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a6772e7055ee427baa7d9f400ef121b6     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  11:35:55 up  2:33,  2 users,  load average: 0.72, 0.32, 0.18

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -17,8 +17,8 @@ static Context* do_event(Event e, Context* c) {
 
     case EVENT_IRQ_TIMER:
       Log("EVENT_IRQ_TIMER happening but doing nothing...");
-      break;
-      //return schedule(c);
+      //break;
+      return schedule(c);
 
     default: panic("Unhandled event ID = %d", e.event);
   }
