5763aee182bade85b988cfa327973a51d84f289c
tracer-ics2021 <tracer@njuics.org>
2022-01-13 11:14:51 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: a6772e7055ee427baa7d9f400ef121b6     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  11:14:51 up  2:12,  2 users,  load average: 0.69, 0.39, 0.21

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -106,7 +106,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
   Context *context = kstack.end - sizeof(Context);
-  context->mstatus = 0xC0000 | 0x8;//MPP设置为U模式，MXR=1，SUM=1
+  context->mstatus = 0xC0000 | 0x80;//MPP设置为U模式，MXR=1，SUM=1
   context->mepc    = (uintptr_t)entry;
   context->pdir    = as->ptr;
 
