005488a2759b26c716205a8f3bf67a1630666914
tracer-ics2021 <tracer@njuics.org>
2022-01-14 20:31:01 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 02feeb7415214ca2a6854ec22cd330af     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  20:31:01 up  1:22,  1 user,  load average: 0.13, 0.10, 0.10

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -48,7 +48,7 @@ Context* schedule(Context *prev) {
 
   // always select pcb[0] as the new process
   current = (current == &pcb[0] ? &pcb[program_index] : &pcb[0]);
-
+  printf("schedule c->pdir内容地址修改后 页表项:%p\t上下文地址%p\n", current->cp->pdir, &current->cp);
   // then return the new context
   return current->cp;
 }
