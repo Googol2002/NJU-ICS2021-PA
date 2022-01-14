6334a4299375f068d28b3026ba1dad847e7fd7b8
tracer-ics2021 <tracer@njuics.org>
2022-01-14 19:45:23 +0800

    >  compile 201300052    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: fe1e0c26c9464c17b65971f217c24851            Boot ID: 02feeb7415214ca2a6854ec22cd330af     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  19:45:23 up 36 min,  1 user,  load average: 0.02, 0.09, 0.19

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,7 +28,7 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg);
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]);
 
 #define PROG_PATH1 "/bin/menu"
-#define PROG_PATH2 "/bin/timer"
+#define PROG_PATH2 "/bin/timer-test"
 #define PROG_PATH3 "/bin/hello"
 void init_proc() {
   context_kload(&pcb[0], hello_fun, "ONE");
